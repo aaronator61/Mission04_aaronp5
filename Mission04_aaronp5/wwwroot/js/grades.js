62b9b3813b0e04de61abfa2fc47c755f0ee27c79
aaron <aaron@PAN-YA-LUN>
2023-01-30 20:21:43 -0700

    Add project files.

---
+++ Mission04_aaronp5/wwwroot/js/grades.js
@@ -0,0 +1,54 @@
+﻿var totalGrade;
+var letterGrade;
+
+//form where user inputs percentages for each section that goes into final grade
+$("#grades").click(function () {
+
+    //Calculating total grade with the individual weights
+    totalGrade = $("#assignments").val() * .5 +
+        $('#groupProject').val() * .1 + 
+        $('#quizzes').val() * .1 +
+        $('#midtermExam').val() * .1 +
+        $('#finalExam').val() * .1 +
+        $('#intex').val() * .1;
+
+    //Determining what letter grade to display
+    if (totalGrade >= 94) {
+        letterGrade = 'A';
+    }
+    else if (totalGrade >= 90) {
+        letterGrade = 'A-';
+    }
+    else if (totalGrade >= 87) {
+        letterGrade = 'B+';
+    }
+    else if (totalGrade >= 84) {
+        letterGrade = 'B';
+    }
+    else if (totalGrade >= 80) {
+        letterGrade = 'B-';
+    }
+    else if (totalGrade >= 77) {
+        letterGrade = 'C+';
+    }
+    else if (totalGrade >= 74) {
+        letterGrade = 'C';
+    }
+    else if (totalGrade >= 70) {
+        letterGrade = 'C-';
+    }
+    else if (totalGrade >= 90) {
+        letterGrade = 'D+';
+    }
+    else if (totalGrade >= 90) {
+        letterGrade = 'D';
+    }
+    else if (totalGrade >= 90) {
+        letterGrade = 'D-';
+    }
+    else {
+        letterGrade = 'E'
+    }
+    $('#calculatedGrade').text("Final Grade: " + totalGrade.toString() + "%  " + letterGrade);
+
+})
